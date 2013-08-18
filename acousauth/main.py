bc1bc459f0e7ce2246496adaffc26ad97ea48d51
quake0day <quake0day@gmail.com>
2013-08-18 01:37:07 -0400

    finished export wav

--- acousauth/main.py
+++ acousauth/main.py
@@ -1,196 +1,135 @@
 #!/usr/bin/python
 # -*-coding:utf-8 -*-
-import time
-import threading
+import time;
+import threading;
 import web
-import pymongo
-from pymongo import MongoClient
-from session import MongoStore
-from web import form
-from web.contrib.template import render_jinja
-import users
-import gridfs
-
-# In order to make session work
-web.config.debug = False
-
-# Session timeout
-web.config.session_parameters['timeout'] = 60 * 60
-
+import sys,wave
+import numpy as np
+from scikits.audiolab import Sndfile, Format
+import struct
 urls = (
-    '/', 'Index',
-    '/index', 'Index',
-    '/submit', 'Submit',
-    '/login', 'Login',
-    '/admin', 'Admin',
-    '/add', 'Add',
-    '/log', 'Log'
+    '/', 'index',
+    '/submit', 'submit'
 )
 
-# Define the template directory
-tmpl = web.template.render("tmpl/")
-
-# Define Jinja2
-render = render_jinja(
-      'templates',  # Jinja2 template directory
-      encoding = 'utf-8',
-    )
-
-# Get mongodb
-client = MongoClient()
-db = client.mydb2
-
-# Get GridFS for avatars
-gfsAvatar = gridfs.GridFS(db)
-
-# Create app object here
-app = web.application(urls, globals())
-
-# Create session
-session = web.session.Session(app, MongoStore(db, 'sessions'))
-users.session = session
-
-# Create mongo db's collection
-users.collection = db.users
-
-class Index:
+i = 0
+class index:
     def GET(self):
+        tmpl = web.template.render("tmpl/")
         return tmpl.index()
 
     def POST(self):
         #data = web.data()
         return "OK"
 
-class Submit:
+
+class submit:
+
+
     def POST(self):
+        global i
         data = web.data()
-        f = open("./temp",'w')
-        f.write(data)
-        f.close()
+        filename = "temp["+str(i)+"].wav"
+        #sig = np.array([0, 1, 0, -1, 0], dtype=np.float32)
+        #f = Sndfile(filename, 'w', Format('wav','pcm32'), 1, 44100)
+        #f.write_frames(data)
+        #f.close()
+        data = data[229:-44]
+        
+        #wavfile = float32_wav_file(data, 44100)
+        #f = open(filename,'wb')
+        #f.write(wavfile)
+        #f.close()
+        #filename = "test.wav"
+        #ifile = wave.open("temp.wav")
+        wavfile = wave.open(filename,'wb')
+        #wavfile.setparams(ifile.getparams())
+        wavfile.setparams((2, 2, 44100, 44100*4, 'NONE', 'not compressed'))
+        #sampwidth = ifile.getsampwidth()
+        #print sampwidth
+        #fmts = (None, "=B", "=h", None, "=l")
+        #fmt = fmts[sampwidth]
+        #dcs = (None, 128, 0, None, 0)
+        #dc = dcs[sampwidth]
+        #for i in range (ifile.getnframes()):
+         #   iframe = 
+        wavfile.writeframes(data)
+        wavfile.close()
+        #f = open(filename,'wb')
+        #f.write(data)
+        #f.close()
+        i = i + 1
         #print data
         return "OK"
 
-class Login:
-  '''
-    Login page: for admin to login
-  '''
-
-  def GET(self):
-    return render.login()
-
-  def POST(self):
-    post = web.input(_method='POST')
-
-    user = users.get_user_by_name(post['username'])
-    if user is None:
-      return 'Not found'
-    elif user['password'] != post['password']:
-      return 'Wrong password'
-    elif user['authority'] != 0:
-      return 'Not admin'
-    else:
-      users.login(user)
-      return web.seeother('/admin')
-
-class Admin:
-  '''
-    Admin page: show all the registered users.
-  '''
-
-  def GET(self):
-    user = users.get_user_by_sid()
-    if user is None:
-      return web.seeother('/login')
-    else:
-      #allUsers = users.get_all_users()
-      #allUsers['create_date'] = str(allUsers['create_date'])
-      #print allUsers['create_date']
-      return render.admin(users=users.get_all_users())
-
-class Add:
-  '''
-    Add page: for admin to add new user
-  '''
-
-  def GET(self):
-    user = users.get_user_by_sid()
-    if user is None:
-      return web.seeother('/login')
-    else:
-      return render.add()
-
-  def POST(self):
-    post = web.input(_method='POST')
-
-    username  = post['username']
-    pwd       = post['password']
-    authority = 0 if post['authority'] == 'Admin' else 1
-
-    # Validate
-    #if username or pwd or pwdagain or authority is None:
-      #return "field blank"
-    if username is None:
-      return 'Please fill the username'
-    elif pwd is None:
-      return 'Please fill the password'
-    elif authority is None:
-      return 'Please fill the authority'
-    elif users.get_user_by_name(username) is not None:
-      return "user exists"
-    else:
-      user = users.add(username=username, password=pwd, authority=authority)
-      return web.seeother('/admin')
-
-class Log:
-  def GET(self):
-    return render.log()
 
 class MTimerClass(threading.Thread):  # cookie监控时钟
-    def __init__(self,fn,args=(),sleep=1):
+    def __init__(self,fn,args=(),sleep=1):        
         threading.Thread.__init__(self)
-        self.fn = fn
-        self.args = args
-        self.sleep = sleep
+        self.fn = fn  
+        self.args = args  
+        self.sleep = sleep 
         self.setDaemon(True)
-
-        self.isPlay = True  #当前是否运行
+        
+        self.isPlay = True  #当前是否运行 
         self.fnPlay = False #当前已经完成运行
-        self.thread_stop=False;
-
+        self.thread_stop=False; 
+        
     def SetSleep(self,sleep): # 重新设置 时间间隔
         self.sleep=sleep;
-
+        
     def __do(self):
         self.fnPlay = True;
         apply(self.fn,self.args);
-        self.fnPlay = False
+        self.fnPlay = False 
 
-    def run(self):
-        while self.isPlay :
+    def run(self): 
+        while self.isPlay : 
             if self.thread_stop==True:
                 break;
             #if SubCommon.ifexeStop==True:  #可以外部调用 来关掉线程。
             #    print 'thread break'
-            #    break;
+            #    break;  
             #print self.sleep;
-            time.sleep(self.sleep)
+            time.sleep(self.sleep) 
             self.__do();
-
-    def stop(self):
-        #stop the loop
+            
+    def stop(self):  
+        #stop the loop  
         self.thread_stop = True;
-        self.isPlay = False;
+        self.isPlay = False; 
         while True:
-            if not self.fnPlay : break
+            if not self.fnPlay : break             
             time.sleep(0.01)
 
 def GetSearchinfo():
     # to do
     pass;
+    
 
-
+def float32_wav_file(sample_array, sample_rate):
+  byte_count = (len(sample_array)) * 4  # 32-bit floats
+  wav_file = ""
+  # write the header
+  wav_file += struct.pack('<ccccIccccccccIHHIIHH',
+    'R', 'I', 'F', 'F',
+    byte_count + 0x2c - 8,  # header size
+    'W', 'A', 'V', 'E', 'f', 'm', 't', ' ',
+    0x10,  # size of 'fmt ' header
+    3,  # format 3 = floating-point PCM
+    1,  # channels
+    sample_rate,  # samples / second
+    sample_rate * 4,  # bytes / second
+    4,  # block alignment
+    32)  # bits / sample
+  wav_file += struct.pack('<ccccI',
+    'd', 'a', 't', 'a', byte_count)
+  for sample in sample_array:
+    wav_file += struct.pack("<f", float(sample))
+  return wav_file
 
 if __name__ == "__main__":
+    app = web.application(urls, globals())
     #tCheck=MTimerClass(GetSearchinfo, '',  10);
     #tCheck.setDaemon(True); # 随主线程一起结果
     #tCheck.start();         #线程启动
